cda9f34ae96028c7ca1fa0d966874ff30a434511
Saj Panchal <sajpanchal@gmail.com>
2021-08-23 12:22:14 -0400

    understanding swift Result type.

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -7,28 +7,51 @@
 
 import SwiftUI
 
+enum NetworkError: Error {
+    case badURL, requestFailed, unknown
+}
+
 struct ContentView: View {
     @ObservedObject var user = User()
     @State var selectedTab = 0
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Text("Tab 1")
-                .onTapGesture {
-                    self.selectedTab = 1
-                }
-                .tabItem {
-                    Image(systemName: "star")
-                    Text("One")
-                }
-                .tag(0)
-            Text("Tab 2")
-               
-                .tabItem {
-                    Image(systemName: "star.fill")
-                    Text("Two")
-                }
-                .tag(1)
+      Text("Hello World")
+        .onAppear {
+            self.fetchData(from: "https://www.apple.com") {
+                result in
+                switch result {
+                    case .success(let str): print(str)
+                    case .failure(let error):
+                        switch error {
+                            case .badURL: print("Bad URL")
+                            case .requestFailed: print("Network problems")
+                            case .unknown: print("Unkonw error")
+                        }
+                    }
+                }            
         }
+    }
+    
+    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
+        // success or failure
+        guard let url = URL(string: urlString) else {
+            completion(.failure(.badURL))
+            return
+        }
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            DispatchQueue.main.async {
+                if let data = data {
+                    // convert data to string
+                    let stringData = String(decoding: data, as: UTF8.self)
+                    completion(.success(stringData))
+                } else if error != nil {
+                    completion(.failure(.requestFailed))
+                } else {
+                    completion(.failure(.unknown))
+                }
+            }
+        }.resume()
+        
         
     }
 }
