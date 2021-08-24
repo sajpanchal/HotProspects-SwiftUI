e6642fa79b12e167830c3a0a6d394b7acc88c076
Saj Panchal <sajpanchal@gmail.com>
2021-08-24 12:09:13 -0400

    added local user notifications.

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import UserNotifications
 
 enum NetworkError: Error {
     case badURL, requestFailed, unknown
@@ -20,6 +21,30 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            Button("Request Permission") {
+                UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.badge,.sound]) { success, error in
+                    if success {
+                        print("All Set")
+                    }
+                    else if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+            }
+            Button("Schedule Notification") {
+                let content = UNMutableNotificationContent()
+                content.title = "Feed the cat"
+                content.subtitle = "It looks hungry"
+                content.sound = UNNotificationSound.default
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
+            }
+        }
+        
+        /*VStack {
             Text("Hello, World!")
                 .padding()
                 .background(backgroundColor)
@@ -44,9 +69,7 @@ struct ContentView: View {
                         Text("Blue")
                     })
                 }
-                   
-                
-        }
+        }*/
         
         //Text("Value is: \(updater.value)")
         /*Text("Hello World")
